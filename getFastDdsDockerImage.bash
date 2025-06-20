2e073767adf9319303129dbc8c7cbeeb4cbee77b
NHK <nhk@eiva.com>
2025-06-20 11:33:15 +0200

    AB#8668 uncommented out the wget part of downloading docker image from eprosima

--- getFastDdsDockerImage.bash
+++ getFastDdsDockerImage.bash
@@ -3,6 +3,6 @@
 
 VERSION="v3.2.2"
 
-#wget "https://www.eprosima.com/component/ars/item/ubuntu-fastdds-$VERSION.tar"
+wget "https://www.eprosima.com/component/ars/item/ubuntu-fastdds-$VERSION.tar"
 
 docker load -i ubuntu-fastdds-$VERSION.tar
